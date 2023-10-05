5fa531a61439a53b82b74be80602d004d6dfefbd
victorhtl <vht.luna@gmail.com>
2023-10-05 11:39:07 -0300

    modulo12-melhoria

---
+++ Módulo 12 - Processo de compilação de um projeto profisisonal/calculadora.h
@@ -0,0 +1,9 @@
+#ifndef _CALC_
+#define _CALC_
+
+int soma(int argv, char *argc[]);
+int multiplica(int argv, char *argc[]);
+int subtrai(int argv, char *argc[]);
+int divide(int argv, char *argc[]);
+
+#endif
